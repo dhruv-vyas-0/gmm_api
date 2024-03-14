822d0a58ea596de402741cebed2a54e8841ee667
unknown <varshitmehta1234@gmail.com>
2024-03-14 10:34:41 +0530

    login bug

--- controllers/login.controller.js
+++ controllers/login.controller.js
@@ -5,14 +5,17 @@ const loginController = {
     login: async (req, res) => {
         try {
             const { email, password } = req.body;
-            const [user_id, temp] = await pool.query("SELECT user_id FROM users WHERE email = ?", [email]);
-            const [rows, fields] = await pool.query("SELECT password, role FROM credentials WHERE user_id = ?;", [user_id[0]]);
+            let [user_id, temp] = await pool.query("SELECT user_id FROM users WHERE email = ?", [email]);
+            user_id = user_id[0].user_id;
+            const [rows, fields] = await pool.query("SELECT password, role FROM credentials WHERE user_id = ?;", [user_id]);
             if (rows.length === 0) {
                 res.status(401).json({
                     message: "No such user"
                 });
             } else if (password == rows[0].password) {
+                console.log(user_id)
                 const [user, fields] = await pool.query("SELECT * FROM users WHERE user_id = ?;", [user_id]);
+                console.log(user)
                 res.status(200).json({
                     message: "Login successfull",
                     role: rows[0].role,
